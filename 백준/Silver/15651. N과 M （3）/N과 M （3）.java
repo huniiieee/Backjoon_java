f61b2ac63e37b8a6331039eb242f48c5eeb93a0c
Lee se Hun <91304749+huniiieee@users.noreply.github.com>
2024-12-27 23:03:25 +0900

    [Silver III] Title: N과 M (3), Time: 492 ms, Memory: 82196 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,42 @@
+
+// don't place package name.
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static int N;
+    public static int M;
+
+    public static void backtracking(StringBuilder temp,int cnt) throws IOException {
+        if(cnt==M){
+            bw.write(temp+"\n");
+            return;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            int len=temp.length();
+            if(cnt>0)
+                temp.append(" ");
+            temp.append(i);
+            backtracking(temp,cnt+1);
+            temp.setLength(len);
+        }
+
+    }
+    public static void main(String[] args) throws IOException {
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        backtracking(new StringBuilder(),0);
+        bw.flush();
+        bw.close();
+    }
+}
