b3f8b74d576562aef5edecc4315a8500ef89bab3
Se hun <sehoon981014@gmail.com>
2025-03-28 09:59:48 +0900

    [Silver III] Title: 이건 꼭 풀어야 해!, Time: 1176 ms, Memory: 138216 KB -BaekjoonHub

---
+++ 백준/Silver/17390. 이건 꼭 풀어야 해！/이건 꼭 풀어야 해！.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+
+    public static BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+    public static BufferedWriter bw= new BufferedWriter(new OutputStreamWriter(System.out));
+    public static int N;
+    public static int Q;
+
+    public static void main(String[] args) throws IOException {
+        String []input=br.readLine().split(" ");
+        N=Integer.parseInt(input[0]);
+        Q=Integer.parseInt(input[1]);
+
+
+        int [] sequence=new int[N];
+        int [] sum=new int[N+1];
+        input=br.readLine().split(" ");
+        for (int i = 0; i < N; i++) {
+            sequence[i]=Integer.parseInt(input[i]);
+        }
+        Arrays.sort(sequence);
+        sum[0]=0;
+        int temp=0;
+        for (int i = 0; i < N; i++) {
+            temp +=sequence[i];
+            sum[i+1]=temp;
+        }
+        for (int i = 0; i < Q; i++) {
+            input=br.readLine().split(" ");
+            int start=Integer.parseInt(input[0]);
+            int end=Integer.parseInt(input[1]);
+            bw.write(sum[end]-sum[start-1]+"\n");
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
