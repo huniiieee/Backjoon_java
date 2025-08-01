fed4a3d123a1ed23d6a72315b497703051a040cf
Se hun <sehoon981014@gmail.com>
2025-08-01 09:56:15 +0900

    [level 2] Title: [3차] n진수 게임, Time: 13.36 ms, Memory: 93.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(int n, int t, int m, int p) {
+        StringBuilder answer = new StringBuilder();
+        
+        StringBuilder sb=new StringBuilder();
+        int idx=0;
+        while(sb.length()<t*m){
+            sb.append(Integer.toString(idx,n).toUpperCase());
+            idx++;
+        }
+        for(int i=p-1;i<t*m;i+=m){
+            answer.append(sb.charAt(i));
+        }
+        return answer.toString();
+    }
+}
