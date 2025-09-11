3e7fe322215d41c2fdab2fd501327b83726f6031
Se hun <sehoon981014@gmail.com>
2025-09-11 10:30:25 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 7.12 ms, Memory: 82.4 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
@@ -1,54 +1,61 @@
 import java.util.*;
 
 class Solution {
-    int []dx={0,-1,0,1};
-    int []dy={-1,0,1,0};
-    int answer=Integer.MAX_VALUE;
-    boolean [][][]visited;
-    public int solution(int[][] board) {
-        int n=board.length;
-        visited=new boolean[n][n][4];
-        
-        bfs(board);
-        return answer;
-    }
+    int[] dx = {0, 1, 0, -1}; // 오른쪽, 아래, 왼쪽, 위
+    int[] dy = {1, 0, -1, 0};
     
-    public void bfs(int [][]board){
-        Queue<int []> q=new LinkedList<>();
-        q.offer(new int[]{0,0,-1,0}); // x,y,dir,cost
-     
-        while(!q.isEmpty()){
-            int []cur=q.poll();
-            int x=cur[0];
-            int y=cur[1];
-            int dir=cur[2];
-            int cost=cur[3];
-            
-            if(x==board.length-1 && y==board.length-1){
-                answer =Integer.min(answer,cost);
-            }
-            
-            for(int i=0;i<4;i++){
-                int nx=x+dx[i];
-                int ny=y+dy[i];
-                if(nx<0||nx>=board.length||ny<0||ny>=board.length||board[nx][ny]==1)
-                    continue;
-                
-            
-                int nc=cost;
-                if(dir==-1 ||dir==i) nc +=100;
-                else nc +=600;
-                
-                if(!visited[nx][ny][i] || board[nx][ny]>=nc){
-                    visited[nx][ny][i]=true;
-                    board[nx][ny]=nc;
-                    q.offer(new int[]{nx,ny,i,nc});
-                }
-                    
-            }
-            
-            
+    class Node {
+        int x, y, dir, cost;
+        Node(int x, int y, int dir, int cost) {
+            this.x = x;
+            this.y = y;
+            this.dir = dir;
+            this.cost = cost;
         }
-        
+    }
+
+    public int solution(int[][] board) {
+        int N = board.length;
+        int[][][] dist = new int[N][N][4]; // 방향별 최소 비용
+        for(int i=0;i<N;i++){
+            for(int j=0;j<N;j++){
+                Arrays.fill(dist[i][j], Integer.MAX_VALUE);
+            }
+        }
+
+        Queue<Node> q = new LinkedList<>();
+        for(int i=0;i<4;i++){
+            dist[0][0][i] = 0;
+            q.offer(new Node(0, 0, i, 0));
+        }
+
+        while(!q.isEmpty()){
+            Node cur = q.poll();
+            int x = cur.x;
+            int y = cur.y;
+            int dir = cur.dir;
+            int cost = cur.cost;
+
+            for(int i=0;i<4;i++){
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+
+                if(nx<0 || nx>=N || ny<0 || ny>=N || board[ny][nx]==1) continue;
+
+                int nc = cost + (dir == i ? 100 : 600); // 직선:100, 코너:600
+
+                if(dist[ny][nx][i] > nc){
+                    dist[ny][nx][i] = nc;
+                    q.offer(new Node(nx, ny, i, nc));
+                }
+            }
+        }
+
+        int answer = Integer.MAX_VALUE;
+        for(int i=0;i<4;i++){
+            answer = Math.min(answer, dist[N-1][N-1][i]);
+        }
+
+        return answer;
     }
 }
